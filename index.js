044d369eb69d3e6cb8bde583ff9a48ac1e895009
Amy Jackson <aj81623@gmail.com>
2017-11-12 18:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+functionlowerCaseArray(array){
+  return array.map(function(e){
+    return e.toLowerCase()
+  })
+  
+}
+
+function nameToAttributes(names){
+  names.map(function(name){
+
+    let fName = name.split("")[0]
+    let lName - name.split("")[1]
+    return {fName, lastName: lName}
+  })
+}
